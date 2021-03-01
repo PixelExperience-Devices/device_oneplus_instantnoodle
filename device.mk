6e000af0c06b838accf15a215f23f9f59d1fdd40
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-01 13:22:06 +0800

    instantnoodle: Set default hotspot ssid by RRO
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I8622743b73109d0c1d8bb2bddece609139546279

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    OnePlus8WifiOverlay \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
