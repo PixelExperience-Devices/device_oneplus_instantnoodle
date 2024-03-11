PixelExperience-Devices/device_oneplus_instantnoodlep

instantnoodlep: Ship along with OnePlus firmware
Change-Id: I0cf47621f39133ce5e41622b27fde72cef7414e0

BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/oneplus/instantnoodlep
 # Display
 TARGET_SCREEN_DENSITY := 450
 
+# Firmware
+-include vendor/oneplus/firmware/BoardConfigVendor.mk
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 

PixelExperience-Devices/device_oneplus_instantnoodle

BoardConfig.mk
@@ -32,4 +32,4 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL := INCLUDE_DIR
 SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(DEVICE_PATH)/touch/include
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/instantnoodle/BoardConfigVendor.mk
+-include vendor/oneplus/instantnoodle/BoardConfigVendor.mk